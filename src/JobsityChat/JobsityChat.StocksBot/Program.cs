ee760faee85d084653b4ff47f3d72811b8b945e5
amanon <angelandresmanon@gmail.com>
2021-02-09 23:12:58 -0400

    adding project architecture

---
+++ src/JobsityChat/JobsityChat.StocksBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobsityChat.StocksBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
