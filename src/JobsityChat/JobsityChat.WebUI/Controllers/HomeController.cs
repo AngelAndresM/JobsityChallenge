3c84a09502c9d292f01e023c27728d901a117de9
Angel Andres Manon <angelandresmanon@gmail.com>
2021-02-15 23:45:45 -0400

    loading latest messages into chat room ui

--- src/JobsityChat/JobsityChat.WebUI/Controllers/HomeController.cs
+++ src/JobsityChat/JobsityChat.WebUI/Controllers/HomeController.cs
@@ -37,11 +37,6 @@ namespace JobsityChat.WebUI.Controllers
             return View(messages);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
